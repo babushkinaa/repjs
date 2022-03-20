8e036cfd2ca33b49ba137f47970d3bb6dff1fffc
lelspb <007-99@mail.ru>
2022-03-20 16:54:34 +0300

    lesson01

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log('hello');
+alert('hello');
+
+let money =1; 
+let income =2; 
+let addExpenses=3; 
+let deposit=4; 
+let mission=5; 
+let period=6;
+
+console.log( money, income, addExpenses, deposit, mission, period);
+console.log(typeof(money));

665165a29b5ba6569322771c5207ac72d190a21c
all lexa <you@example.com>
2023-10-31 18:14:40 +0300

    Lesson2

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
-console.log('hello');
-alert('hello');
+console.log("hello");
+//alert("hello");
 
-let money =1; 
-let income =2; 
-let addExpenses=3; 
-let deposit=4; 
-let mission=5; 
-let period=6;
-let period1=61;
+let money = 10000; // доход за месяц
+let income = 2150; // дополнительный доход
+let addExpenses = "инетернет, телефон, квартплата, машина"; // расходы через запятую
+let deposit = true; // булево значение
+let mission = 1000000; // что хотим накопить
+let period = 11; // период накопления 1-12 месяцев
+let budgetDay = 500; // дневной бюджет
 
-console.log( money, income, addExpenses, deposit, mission, period);
-console.log(typeof(money),typeof(income), typeof(addExpenses));
+console.log(money, income, deposit + " -типы переменных");
+console.log(
+	"Длинна строки { addExpenses }" + " " + addExpenses.length + " символов"
+);
+console.log("Период равен: " + period + " месяцев");
+console.log("Цель заработать: " + mission + " рублей");
+console.log(addExpenses.split(",").reverse().join(" "));
+console.log(addExpenses.toUpperCase());
+console.log("Дневной бюджет: " + budgetDay + " рублей");
